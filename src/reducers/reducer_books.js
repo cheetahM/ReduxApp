713b401376f2ef4722289b0334e553a20b5c3087
cmeshram <meshram.chetan@gmail.com>
2017-01-05 00:36:38 -0800

    Adding book details react component

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Imposters syndrome'},
-    {title: 'You dont know JavaScript'},
-    {title: 'Meditations'},
-    {title: 'Ego is the enemy'}
+    {title: 'Imposters syndrome', pages: 300},
+    {title: 'You dont know JavaScript', pages: 200},
+    {title: 'Meditations', pages:150},
+    {title: 'Ego is the enemy', pages: 100}
   ]
 }
